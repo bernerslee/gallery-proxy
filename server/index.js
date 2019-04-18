bed3e87837a7032810d949b8b4d2eef1ad6ef792
Felipe <phil.sousa@gmail.com>
2019-04-18 14:45:40 -0700

    included yml file for artillery and newrelic.js for new relic metrics

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
+require('newrelic');
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var morgan = require('morgan');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(morgan('dev'))
+
 
 app.use('/:id', express.static(__dirname+'/../client'));
 // app.use(express.static(__dirname+'/../client'));
